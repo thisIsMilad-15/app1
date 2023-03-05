cedb236080c7ca0fa668af346a6f6b27ff1ffd7d
thisIsMilad-15 <norozzadehm129@gmail.com>
2023-03-05 09:11:19 +0330

    add student class

---
+++ app1/student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace app1
+{
+    internal class student
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+
+    }
+}
